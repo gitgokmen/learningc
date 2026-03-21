03c5ce05036df6ad943741fd9ffb630ac7873ad1
gokmeninfo@gmail.com <gokmeninfo@gmail.com>
2026-03-21 22:35:57 +0300

    Day One

--- hello.c
+++ hello.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    printf("Hello world!");
+    int age = 25;
+    float gpa = 3.73;
+    char grade = 'A';
+    printf("You are %d years old.\n", age);
+    printf("Your gpa is %.1f\n", gpa);
+    printf("Your grade is %c \n", grade);
+
+    char name[] = "Gokmen Budak";
+
+    printf("Hello, %s\n", name);
+
+    bool isOnline = true;
+
+    if (isOnline)
+    {
+        printf("You are ONLINE!");
+    }
+    else
+    {
+        printf("You are OFFLINE!");
+    }
+
+    return 0;
 }
