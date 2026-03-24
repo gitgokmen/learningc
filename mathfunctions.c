56c3745bc7c535a7d75f78ba7547d30a256dbea7
gokmeninfo@gmail.com <gokmeninfo@gmail.com>
2026-03-24 21:51:37 +0300

    Updated Day 3

---
+++ mathfunctions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    float x = 3.14;
+    // x = sqrt(x);
+
+    // x = pow(x, 3);
+
+    // x = round(x); rounds depends on the number
+
+    // x = ceil(x); always runs up
+
+    // x = floor(x); rounds always down
+
+    // x = abs(x); Absoulte value function
+    printf("%.2f", x);
+
+    return 0;
+}
